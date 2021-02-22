2904e14202ec7ae297713ca5b45cf5128f94f149
Stas <banfan24@gmail.com>
2021-02-22 12:51:50 +0200

    project

---
+++ app/js/main.js
@@ -0,0 +1,10 @@
+!function(e){var r={};function n(_){if(r[_])return r[_].exports;var t=r[_]={i:_,l:!1,exports:{}};return e[_].call(t.exports,t,t.exports,n),t.l=!0,t.exports}n.m=e,n.c=r,n.d=function(e,r,_){n.o(e,r)||Object.defineProperty(e,r,{enumerable:!0,get:_})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,r){if(1&r&&(e=n(e)),8&r)return e;if(4&r&&"object"==typeof e&&e&&e.__esModule)return e;var _=Object.create(null);if(n.r(_),Object.defineProperty(_,"default",{enumerable:!0,value:e}),2&r&&"string"!=typeof e)for(var t in e)n.d(_,t,function(r){return e[r]}.bind(null,t));return _},n.n=function(e){var r=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(r,"a",r),r},n.o=function(e,r){return Object.prototype.hasOwnProperty.call(e,r)},n.p="",n(n.s="./src/js/main.js")}({"./src/js/func.js":
+/*!************************!*\
+  !*** ./src/js/func.js ***!
+  \************************/
+/*! exports provided: asd */function(module,__webpack_exports__,__webpack_require__){"use strict";eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"asd\", function() { return asd; });\nfunction asd() {\n  console.log('asdsdfs');\n}\n\n//# sourceURL=webpack:///./src/js/func.js?")},"./src/js/main.js":
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+/*! no exports provided */function(module,__webpack_exports__,__webpack_require__){"use strict";eval('__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _func__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./func */ "./src/js/func.js");\n\nObject(_func__WEBPACK_IMPORTED_MODULE_0__["asd"])();\n\n//# sourceURL=webpack:///./src/js/main.js?')}});
+//# sourceMappingURL=main.js.map
